815914e20bc6239578feeef4fb5d0b3b3585bcca
Roddy99-Jaramillo <52213222+Roddy99-Jaramillo@users.noreply.github.com>
2020-01-09 09:05:02 -0500

    Create numeros_primos.cpp
    roddy

---
+++ numeros_primos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <thread>
+#include<fstream>
+#include<conio.h>
+#include <pthread.h> 
+#include <unistd.h> 
+ #include<windows.h>
+#include <string.h>
+int numero;
+int i;
+void *primo(void*);
+int contador=0;
+
